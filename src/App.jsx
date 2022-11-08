c0f5745b2a786473654aee49381b7f665d516ff2
pooh <cktjdgus45@naver.com>
2022-11-08 19:04:31 +0900

    send props to profile

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Profile from './components/Profile';
+
+function App() {
+  return (
+    <div className="App">
+      <Profile
+        image="https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
+        name="POPO"
+        job="프론트엔드 개발자"
+      />
+      <Profile
+        image="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=922&q=80"
+        name="POPOlo"
+        job="백엔드 개발자"
+      />
+      <Profile
+        image="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80"
+        name="POPOry"
+        job="데브옵스엔드 개발자"
+      />
+    </div>
+  );
+}
+
+export default App;
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Profile = () => {
+const Profile = (props) => {
     return (
         <div className='profile'>
-            <img className='photo' src="https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80" alt="avatar" />
-            <h1>popo</h1>
-            <p>프론트엔드 개발자</p>
+            <img className='photo' src={props.image} alt="avatar" />
+            <h1>{props.name}</h1>
+            <p>{props.job}</p>
         </div>
     )
 }
