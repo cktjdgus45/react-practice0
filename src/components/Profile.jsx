ef2ca36c167e53ab66eb486e19578041b5d7f77b
pooh <cktjdgus45@naver.com>
2022-11-08 20:19:19 +0900

    add new tag to profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,6 +4,7 @@ const Profile = (props) => {
     return (
         <div className='profile'>
             <img className='photo' src={props.image} alt="avatar" />
+            {props.isNew && <span className='new shown'>New</span>}
             <h1>{props.name}</h1>
             <p>{props.job}</p>
         </div>
