8a96b74187d5c21bb5146b4b3349bbe3a5ae9fed
pooh <cktjdgus45@naver.com>
2022-11-14 14:31:46 +0900

    practice context

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
-import Counter from './components/Counter';
+import React, { useContext } from 'react';
+
+const themes = {
+  light: {
+    foreground: "#000000",
+    background: "#eeeeee"
+  },
+  dark: {
+    foreground: "#ffffff",
+    background: "#222222"
+  }
+};
+
+const ThemeContext = React.createContext(themes);
 
 function App() {
   return (
-    <div className="App">
-      <Counter />
+    <ThemeContext.Provider value={themes.dark}>
+      <Toolbar />
+    </ThemeContext.Provider>
+  );
+}
+
+function Toolbar(props) {
+  return (
+    <div>
+      <ThemedButton />
     </div>
+  );
+}
+
+function ThemedButton() {
+  const theme = useContext(ThemeContext); //사용하는 곳에서 useContext
+  return (
+    <button style={{ background: theme.background, color: theme.foreground }}>
+      I am styled by theme context!
+    </button>
   );
 }
 
